975c3adf26d97a336aa522a0fa59181a7a367778
WindowsVM <s1186192@student.windesheim.nl>
2023-05-28 13:59:28 +0200

    Geupdate database.java vanaf WindowsVM

--- src/Database.java
+++ src/Database.java
@@ -2,12 +2,12 @@ import java.sql.*;
 
 public class Database {
     public static void main(String[] args) {
-        String url="jdbc:mysql://192.168.1.108:3306/NerdyGadgetsV2?user=root&password=Stoma123?";
-        String username="student";
-        String password="Stoma123?";
+        String url="jdbc:mysql://192.168.1.108:3306/NerdyGadgetsV2";
+        String username="GadgetMaster";
+        String password="Poepzakje123?";
         try {
-            Connection connection = DriverManager.getConnection(url);
-            System.out.println("verbonden");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connectie geslaagd");
 
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Component");
