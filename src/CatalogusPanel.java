b3494765803f55733aa281670218679fe3b8041c
WindowsVM <s1186192@student.windesheim.nl>
2023-05-29 14:20:27 +0200

    Ophalen en nieuw component gaat via db !Bug: panel herlaadt niet wanneer nieuw component wordt toegevoegd

--- src/CatalogusPanel.java
+++ src/CatalogusPanel.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 
 public class CatalogusPanel extends Panel implements ActionListener, MouseListener {
     public int geselecteerdComponentID;
+    public String componentNaam;
     public ArrayList<Component> catalogusComponenten;
     public JMenuItem jmiToevoegen;
+    public JMenuItem jmiVerwijderen;
 
     public CatalogusPanel(ArrayList<Component> componenten) {
         super(componenten);
@@ -17,31 +19,12 @@ public class CatalogusPanel extends Panel implements ActionListener, MouseListen
 
         catalogusComponenten = new ArrayList<>();
         Database.haalComponentenOp(catalogusComponenten);
-
-//        // Het initialiseren van de verschillende componenten
-//        Component pfsense = new Component("pfSense", 4000, 99.9980, ComponentType.PFSENSE);
-//        Component mySQL1 = new Component("HAL9001DB", 5100, 90, ComponentType.DATABASESERVER);
-//        Component mySQL2 = new Component("HAL9002DB", 7700, 95, ComponentType.DATABASESERVER);
-//        Component mySQL3 = new Component("HAL9003DB", 12200, 98, ComponentType.DATABASESERVER);
-//        Component apacheServer1 = new Component("HAL9001W", 2200, 80, ComponentType.WEBSERVER);
-//        Component apacheServer2 = new Component("HAL9002W", 3200, 90, ComponentType.WEBSERVER);
-//        Component apacheServer3 = new Component("HAL9003W", 5100, 95, ComponentType.WEBSERVER);
-//
-//
-//        // Het toevoegen van de componenten aan de catelogus
-//        catalogusComponenten.add(pfsense);
-//        catalogusComponenten.add(apacheServer1);
-//        catalogusComponenten.add(apacheServer2);
-//        catalogusComponenten.add(apacheServer3);
-//        catalogusComponenten.add(mySQL1);
-//        catalogusComponenten.add(mySQL2);
-//        catalogusComponenten.add(mySQL3);
-
         tekenCatelogus();
     }
 
     // Deze functie tekent alle componenten binnen de catelogus en zorgt ervoor dat rechtermuisknop menu werkt
-    private void tekenCatelogus() {
+    public void tekenCatelogus() {
+        removeAll();
         for (int i = 0; i < catalogusComponenten.size(); i++) {
 
             // Het aanmaken van een panel zodat alle gegevens van een component bij elkaar blijft
@@ -77,20 +60,21 @@ public class CatalogusPanel extends Panel implements ActionListener, MouseListen
             // Het toevoegen van het component
             jlAfbeelding.addMouseListener(this);
             add(component);
-
-            // MouseListener aan afbeeldingslabel koppelen
-
         }
+        revalidate();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == jmiToevoegen) {
+        if (e.getSource() == jmiToevoegen) {
             Component voorbeeldComponent = catalogusComponenten.get(geselecteerdComponentID);
             Component component = new Component(voorbeeldComponent.getNaam(), voorbeeldComponent.getKosten(), voorbeeldComponent.getBeschikbaarheid(), voorbeeldComponent.getType());
 
             SimulatieFrame.visualisatiePanel.voegComponentToe(component);
             repaint();
+        } else if (e.getSource() == jmiVerwijderen) {
+            Database.verwijderComponent(componentNaam);
+            SimulatieFrame.catalogusPanel.catalogusComponenten.remove(componentNaam);
         }
     }
 
@@ -101,12 +85,19 @@ public class CatalogusPanel extends Panel implements ActionListener, MouseListen
             jmiToevoegen = new JMenuItem("Toevoegen");
             jmiToevoegen.addActionListener(SimulatieFrame.catalogusPanel);
 
+            jmiVerwijderen = new JMenuItem("Verwijderen");
+            jmiVerwijderen.addActionListener(SimulatieFrame.catalogusPanel);
+
             JLabel clickedLabel = (JLabel) e.getComponent(); // Haal de geklikte label op
             String imageId = clickedLabel.getName(); // Haal de bijbehorende ID bij het label op
             int id = Integer.parseInt(imageId);
             geselecteerdComponentID = id;
 
+            Component geselecteerdComponent = catalogusComponenten.get(geselecteerdComponentID);
+            componentNaam = geselecteerdComponent.getNaam();
+
             popupMenu.add(jmiToevoegen);
+            popupMenu.add(jmiVerwijderen);
 
             // Popup laten zien op de coordinaten van de cursor
             popupMenu.show(e.getComponent(), e.getX(), e.getY());
